0b9510eece19d4b7f8d501fc393ac87a54d9e15b
Jay Patel <jay734883@gmail.com>
2022-08-13 10:13:18 +0530

    added org pass

--- models/userSchema.js
+++ models/userSchema.js
@@ -56,7 +56,7 @@ userSchema.pre("save", async function (next) {
 
     if (this.isModified("password")) {
         this.password = await bcrypt.hash(this.password, 12);
-        this.cpassword = await bcrypt.hash(this.cpassword, 12);
+        // this.cpassword = await bcrypt.hash(this.cpassword, 12);
     }
     next()
 });
